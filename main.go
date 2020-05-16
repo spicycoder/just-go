a1e2406a2a91fc0b2a5eeb0cb06375f111cef875
SpicyCoder <spk2naveen@hotmail.com>
2020-05-17 00:59:37 +0530

    Map

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(slice)
-	fmt.Println(slice[:])
-	fmt.Println(slice[1:])
-	fmt.Println(slice[:4])
-	fmt.Println(slice[2:3])
+	m := map[string]int{"foo": 18}
+	fmt.Println(m)
+	fmt.Println(m["foo"])
+
+	m["foo"] = 42
+	fmt.Println(m["foo"])
+
+	delete(m, "foo")
+	fmt.Println(m)
 }
